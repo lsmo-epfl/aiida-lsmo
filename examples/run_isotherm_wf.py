efd0dca9417cd73875caa43d6bca61c9a5c5afea
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-11-28 09:10:14 +0100

    Modify the Isotherm WorkChain:
    1) Now the WorkChain does only Zeo++ block pockets calc
    2) Helium void fractions is set to 0 for all raspa calcs
    3) Outputs now have two links: output_parapmeters and
    block_pockets

--- examples/run_isotherm_wf.py
+++ examples/run_isotherm_wf.py
@@ -2,33 +2,19 @@ import os
 import numpy as np
 from aiida.common.example_helpers import test_and_get_code  # noqa
 from aiida.orm import CalculationFactory, DataFactory
-from aiida.orm.data.base import Float, Str
+from aiida.orm.data.base import Float
 from aiida.work.run import submit
 
 from ase.io import read
-from workflows.isotherm import Isotherm
+from aiida_lsmo_workflows.isotherm import Isotherm
 
 # data objects
 ArrayData = DataFactory('array')
 ParameterData = DataFactory('parameter')
 CifData = DataFactory('cif')
 
+#structure = load_node(6952)
 structure = CifData(file=os.getcwd()+'/Cu-MOF-74.cif')
-
-cp2k_options = {
-    "resources": {
-        "num_machines": 2,
-    },
-    "max_wallclock_seconds": 1 * 60 * 60,
-    }
-
-ddec_options = {
-    "resources": {
-        "num_machines": 1,
-    },
-    "max_wallclock_seconds": 1 * 60 * 60 / 2,
-    "withmpi": False,
-    }
 
 # option for zeo++ and raspa
 zr_options = {
@@ -75,29 +61,22 @@ raspa_parameters = ParameterData(dict={
         }],
         })
 
-cp2k_code = test_and_get_code('cp2k@fidis-debug', expected_code_type='cp2k')
-ddec_code = test_and_get_code('ddec@fidis-debug', expected_code_type='ddec')
 zeopp_code = test_and_get_code('zeopp@deneb', expected_code_type='zeopp.network')
 raspa_code = test_and_get_code('raspa@deneb', expected_code_type='raspa')
 
 
 pressures = ArrayData()
-pressures.set_array("pressures", np.array([0.01e5, 0.05e5, 0.1e5, 0.15e5, 0.2e5]))
+pressures.set_array("pressures", np.array([0.01e5]))
 submit(Isotherm,
         structure=structure,
-        probe_molecule=ParameterData(dict={"sigma":1.525}),
+        probe_radius=Float(1.525),
         pressures=pressures,
         min_cell_size=Float(10.0),
-        cp2k_code=cp2k_code,
-        _cp2k_options=cp2k_options,
-        ddec_code=ddec_code,
-        _ddec_options=ddec_options,
         zeopp_code=zeopp_code,
         _zeopp_options=zr_options,
         raspa_code=raspa_code,
         raspa_parameters=raspa_parameters,
         _raspa_options=zr_options,
         _usecharges=True,
-        _guess_multiplicity=True,
         _label='Isotherm',
         )
