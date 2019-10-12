3504c4955b515046d02b685634cd3e2d31436df9
Daniele Ongari <daniele.ongari@gmail.com>
2019-10-12 18:36:52 +0200

    add IsothremCalcPE work chain

--- aiida_lsmo/workchains/__init__.py
+++ aiida_lsmo/workchains/__init__.py
@@ -1,5 +1,6 @@
 """Workchains developed at LSMO laboratory."""
 from .isotherm import IsothermWorkChain
 from .isotherm_multi_temp import IsothermMultiTempWorkChain
+from .isotherm_calc_pe import IsothermCalcPEWorkChain
 from .zeopp_multistage_ddec import ZeoppMultistageDdecWorkChain
 from .multistage_ddec import MultistageDdecWorkChain
