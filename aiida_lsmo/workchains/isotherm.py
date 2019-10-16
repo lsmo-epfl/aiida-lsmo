9722580b018632a6d5955dd15c57c47992996fa5
Daniele Ongari <daniele.ongari@gmail.com>
2019-10-16 14:48:02 +0200

    fix output requirements IsothermWC

--- aiida_lsmo/workchains/isotherm.py
+++ aiida_lsmo/workchains/isotherm.py
@@ -218,7 +218,7 @@ class IsothermWorkChain(WorkChain):
             ),
         )
 
-        spec.expose_outputs(ZeoppCalculation)
+        spec.expose_outputs(ZeoppCalculation, include=['block'])  #only if porous
 
         spec.output(
             'geometric_output',
