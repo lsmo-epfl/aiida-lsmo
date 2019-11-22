99572dd5af48d6e4e66ee18a5323927cc667a804
Daniele Ongari <daniele.ongari@gmail.com>
2019-11-22 15:03:36 +0100

    fix for RASPA mg/g typo

--- aiida_lsmo/calcfunctions/working_cap.py
+++ aiida_lsmo/calcfunctions/working_cap.py
@@ -10,6 +10,7 @@ from aiida.orm import Dict
 def calc_ch4_working_cap(isot_dict):  # Move this to aiida-lsmo/calcfunctions/calc_working_cap
     """Compute the CH4 working capacity from the output_parameters Dict of IsothermWorkChain.
     This must have run calculations at 5.8 and 65.0 bar (at 298K), which are the standard reference for the evaluation.
+
     The results can be compared with Simon2015 (10.1039/C4EE03515A).
     """
     from math import sqrt
@@ -22,10 +23,9 @@ def calc_ch4_working_cap(isot_dict):  # Move this to aiida-lsmo/calcfunctions/ca
         ip65 = isot_dict["isotherm"]["pressure"].index(65.0)
 
         # in iso_dict uptakes are in mol/kg and enthalpies in kJ/mol
-        conv1 = isot_dict["conversion_factor_molec_uc_to_cm3stp_cm3"] / \
-                isot_dict["conversion_factor_molec_uc_to_mol_kg"]
-        conv2 = isot_dict["conversion_factor_molec_uc_to_gr_gr"] / \
-                isot_dict["conversion_factor_molec_uc_to_mol_kg"]
+        conv1 = isot_dict["conversion_factor_molec_uc_to_cm3stp_cm3"] / isot_dict["conversion_factor_molec_uc_to_mol_kg"]  # pylint: disable=line-too-long
+        # BE CAREFULL: "molec_uc_to_gr_gr" is in "reality molec_uc_to_mg_gr"
+        conv2 = isot_dict["conversion_factor_molec_uc_to_gr_gr"] / isot_dict["conversion_factor_molec_uc_to_mol_kg"] / 10  # pylint: disable=line-too-long
 
         wc_65bar_average = isot_dict["isotherm"]["loading_absolute_average"][ip65] - isot_dict["isotherm"][
             "loading_absolute_average"][ip5]
@@ -48,9 +48,9 @@ def calc_ch4_working_cap(isot_dict):  # Move this to aiida-lsmo/calcfunctions/ca
             "wc_65bar_cm3stp/cm3_average": wc_65bar_average * conv1,
             "wc_65bar_cm3stp/cm3_dev": wc_65bar_dev * conv1,
             "wc_65bar_cm3stp/cm3_unit": 'cm3 STP/cm3',
-            "wc_65bar_g/g_average": wc_65bar_average * conv2,
-            "wc_65bar_g/g_dev": wc_65bar_dev * conv2,
-            "wc_65bar_g/g_unit": 'g/g',
+            "wc_65bar_wt%_average": wc_65bar_average * conv2,
+            "wc_65bar_wt%_dev": wc_65bar_dev * conv2,
+            "wc_65bar_wt%_unit": 'g/g/100',
             "wc_65bar_mol/kg_average": wc_65bar_average,
             "wc_65bar_mol/kg_dev": wc_65bar_dev,
             "wc_65bar_mol/kg_unit": 'mol/kg',
@@ -64,9 +64,13 @@ def calc_ch4_working_cap(isot_dict):  # Move this to aiida-lsmo/calcfunctions/ca
 def calc_h2_working_cap(isot_dict):  # Move this to aiida-lsmo/calcfunctions/calc_working_cap
     """Compute the H2 working capacity from the output_parameters Dict of MultiTempIsothermWorkChain.
     This must have run calculations at 1, 5 and 100 bar at 77, 198, 298 K.
+    The US DOE Target for the Onboard Storage of Hydrogen Vehicles set the bar to 4.5 wt% and 30 g/L (Kapelewski2018).
     Case-A: near-ambient-T adsorption, 100bar/198K to 5bar/298K (cf. Kapelewski2018, 10.1021/acs.chemmater.8b03276)
+    ....... Ni2(m-dobdc), experimental: 23.0 g/L
     Case-B: low T adsorption, 100-5bar at 77K (cf. Ahmed2019, 10.1038/s41467-019-09365-w)
+    ....... NU-100, best experimental: 35.5 g/L
     Case-C: low T adsorption at low discharge, 100-1bar at 77K (cf. Thornton2017, 10.1021/acs.chemmater.6b04933)
+    ....... hypMOF-5059389, best simulated: 40.0 g/L
     """
     from math import sqrt
 
@@ -84,10 +88,13 @@ def calc_h2_working_cap(isot_dict):  # Move this to aiida-lsmo/calcfunctions/cal
         case2pt = {"a": [[100, 198], [5, 298]], "b": [[100, 77], [5, 77]], "c": [[100, 77], [1, 77]]}
 
         unitconv = {
-            "wt%": isot_dict["conversion_factor_molec_uc_to_gr_gr"] / \
-                   isot_dict["conversion_factor_molec_uc_to_mol_kg"] * 100, # mol/kg to %wt
-            "g/L": isot_dict["conversion_factor_molec_uc_to_gr_gr"] / \
-                    isot_dict["conversion_factor_molec_uc_to_mol_kg"] * isot_dict["Density"] * 1000 # mol/kg to g/L
+            # BE CAREFULL: "molec_uc_to_gr_gr" is in "reality molec_uc_to_mg_gr"
+            "wt%":
+                isot_dict["conversion_factor_molec_uc_to_gr_gr"] / isot_dict["conversion_factor_molec_uc_to_mol_kg"] /
+                10,  # mol/kg to wt%
+            "g/L":
+                isot_dict["conversion_factor_molec_uc_to_gr_gr"] / isot_dict["conversion_factor_molec_uc_to_mol_kg"] *
+                isot_dict["Density"]  # mol/kg to g/L
         }
 
         out_dict = {}
