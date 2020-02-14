08e3a88ec5decc52e1a1db5e1d3898bbb4fa3d5f
Daniele Ongari <daniele.ongari@gmail.com>
2020-02-14 09:40:38 +0100

    1) fix MultistageDdecWorkChain to restart from WFN 2) use MultistageDdecWorkChain as subwc of ZeoppMultistageDdecWorkChain 3) remove deprecated ZeoppMultistageDdecPEWorkChain: now use NanoporousScreening1WorkChain

--- aiida_lsmo/workchains/zeopp_multistage_ddec.py
+++ aiida_lsmo/workchains/zeopp_multistage_ddec.py
@@ -7,9 +7,9 @@ from aiida.plugins import CalculationFactory, DataFactory, WorkflowFactory
 from aiida.common import AttributeDict
 from aiida.engine import WorkChain, ToContext
 from aiida_lsmo.utils import get_structure_from_cif
+
 # import sub-workchains
-Cp2kMultistageWorkChain = WorkflowFactory('cp2k.multistage')  # pylint: disable=invalid-name
-Cp2kDdecWorkChain = WorkflowFactory('ddec.cp2k_ddec')  # pylint: disable=invalid-name
+MultistageDdecWorkChain = WorkflowFactory('cp2k.multistageddec')  # pylint: disable=invalid-name
 
 # import calculations
 DdecCalculation = CalculationFactory('ddec')  # pylint: disable=invalid-name
@@ -37,25 +37,20 @@ class ZeoppMultistageDdecWorkChain(WorkChain):
         """Define workflow specification."""
         super(ZeoppMultistageDdecWorkChain, cls).define(spec)
 
-        spec.expose_inputs(ZeoppCalculation, namespace='zeopp', exclude=['parameters', 'structure'])
+        spec.input('structure', valid_type=CifData, help='input structure')
         spec.input('zeopp.parameters',
                    valid_type=NetworkParameters,
                    default=NetworkParameters(dict=ZEOPP_PARAMETERS_DEFAULT),
                    required=False,
                    help='parameters for zeo++')
-        spec.input('structure', valid_type=CifData, help='input structure')
-        spec.expose_inputs(Cp2kMultistageWorkChain, exclude=['structure'])
-        spec.expose_inputs(Cp2kDdecWorkChain, exclude=['cp2k_base'])
+        spec.expose_inputs(ZeoppCalculation, namespace='zeopp', exclude=['parameters', 'structure'])
+        spec.expose_inputs(MultistageDdecWorkChain, exclude=['structure'])
 
-        spec.outline(cls.run_zeopp_before, cls.run_cp2kmultistage, cls.run_cp2kddec, cls.run_zeopp_after,
-                     cls.return_results)
+        spec.outline(cls.run_zeopp_before, cls.run_multistageddec, cls.run_zeopp_after, cls.return_results)
 
         spec.expose_outputs(ZeoppCalculation, namespace='zeopp_before_opt', include=['output_parameters'])
-
         spec.expose_outputs(ZeoppCalculation, namespace='zeopp_after_opt', include=['output_parameters'])
-
-        spec.expose_outputs(Cp2kMultistageWorkChain, exclude=['output_structure'])
-        spec.expose_outputs(Cp2kDdecWorkChain, include=['structure_ddec'])
+        spec.expose_outputs(MultistageDdecWorkChain)
 
     def run_zeopp_before(self):
         """ un Zeo++ for the starting structure"""
@@ -83,33 +78,18 @@ class ZeoppMultistageDdecWorkChain(WorkChain):
         self.report("Running Zeo++ calculation <{}>".format(running_zeopp.pk))
         return ToContext(zeopp_after=running_zeopp)
 
-    def run_cp2kmultistage(self):
+    def run_multistageddec(self):
         """Run CP2K-Multistage"""
-        cp2k_ms_inputs = AttributeDict(self.exposed_inputs(Cp2kMultistageWorkChain))
+        cp2k_ms_inputs = AttributeDict(self.exposed_inputs(MultistageDdecWorkChain))
         cp2k_ms_inputs['structure'] = get_structure_from_cif(self.inputs.structure)
-        cp2k_ms_inputs['metadata']['call_link_label'] = 'call_cp2kmultistage'
+        cp2k_ms_inputs['metadata']['call_link_label'] = 'call_multistageddec'
 
-        running = self.submit(Cp2kMultistageWorkChain, **cp2k_ms_inputs)
-        return ToContext(ms_wc=running)
-
-    def run_cp2kddec(self):
-        """Pass the Cp2kMultistageWorkChain outputs as inputs for
-        Cp2kDdecWorkChain: cp2k_base (metadata), cp2k_params, structure and WFN.
-        """
-        cp2k_ddec_inputs = AttributeDict(self.exposed_inputs(Cp2kDdecWorkChain))
-        cp2k_ddec_inputs['cp2k_base'] = self.exposed_inputs(Cp2kMultistageWorkChain)['cp2k_base']
-        cp2k_ddec_inputs['cp2k_base']['cp2k']['parameters'] = self.ctx.ms_wc.outputs.last_input_parameters
-        cp2k_ddec_inputs['cp2k_base']['cp2k']['structure'] = self.ctx.ms_wc.outputs.output_structure
-        cp2k_ddec_inputs['cp2k_base']['cp2k']['parent_calc_folder'] = self.ctx.ms_wc.outputs.remote_folder
-        cp2k_ddec_inputs['metadata']['call_link_label'] = 'call_cp2kddec'
-
-        running = self.submit(Cp2kDdecWorkChain, **cp2k_ddec_inputs)
-        return ToContext(cp2k_ddec_wc=running)
+        running = self.submit(MultistageDdecWorkChain, **cp2k_ms_inputs)
+        return ToContext(msddec_wc=running)
 
     def return_results(self):
         """Return exposed outputs"""
-        self.out_many(self.exposed_outputs(self.ctx.ms_wc, Cp2kMultistageWorkChain))
-        self.out_many(self.exposed_outputs(self.ctx.cp2k_ddec_wc, Cp2kDdecWorkChain))
         self.out_many(self.exposed_outputs(self.ctx.zeopp_before, ZeoppCalculation, namespace='zeopp_before_opt'))
+        self.out_many(self.exposed_outputs(self.ctx.msddec_wc, MultistageDdecWorkChain))
         self.out_many(self.exposed_outputs(self.ctx.zeopp_after, ZeoppCalculation, namespace='zeopp_after_opt'))
-        self.report("WorkChain terminated correctly")
+        self.report("WorkChain terminated correctly.")
