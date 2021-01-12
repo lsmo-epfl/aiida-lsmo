5ceff5dba06c01cc0b8e85bfdad977612613b190
Leopold Talirz <leopold.talirz@gmail.com>
2021-01-12 14:22:30 +0100

    don't install tests/ as package (#67)

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 """Setting up aiida-lsmo for AiiDA"""
 
 import json
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 def run_setup():
     """Provide static information in setup.json such that it can be discovered automatically."""
     with open('setup.json', 'r') as info:
         kwargs = json.load(info)
-    setup(packages=find_packages(),
+    setup(packages=['aiida_lsmo'],
           long_description=open('README.md').read(),
           long_description_content_type='text/markdown',
           **kwargs)
