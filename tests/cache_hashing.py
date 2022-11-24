c836fb82e3c614d6fe10df0c4c9ee0b5dc26475f
Chris Sewell <chrisj_sewell@hotmail.com>
2022-11-24 19:48:17 +0100

    Update cache_hashing.py

--- tests/cache_hashing.py
+++ tests/cache_hashing.py
@@ -8,4 +8,7 @@ class CustomInputHasher(InputHasher):
 
     def __call__(self, cwd: Path) -> str:
         hash_string = super().__call__(cwd)
+        if hash_string == '805a68cedb2a2d30bab0ee403bc9abed':
+            # temporary fix for: https://github.com/lsmo-epfl/aiida-lsmo/issues/102
+            return 'e323f1f24dd00a44b9c973208dcd63d8'
         return hash_string
